5ad6545a1fac84a5c6ac7c9b0b457152fbed801e
Doğan Ali Yeniacun <alidoganyeniacun@gmail.com>
2024-06-21 21:43:14 +0300

    revise network controller

--- lib/src/features/splash/view_model/splash_view_model.dart
+++ lib/src/features/splash/view_model/splash_view_model.dart
@@ -1,35 +1,21 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
+import '../../../product/network/network_controller.dart';
 import '../../../product/routes/app_routes.dart';
-import '../../../product/widgets/custom_snackbar.dart';
+
 
 class SplashViewModel extends GetxController {
-  final Connectivity _connectivity = Connectivity();
+  late final NetworkController _networkController;
 
   @override
   void onInit() {
     super.onInit();
     FlutterNativeSplash.remove();
-    _connectivity.onConnectivityChanged.listen(_updateStatus);
+    _networkController = Get.put(NetworkController(isConnected: _goToTheHomePage));    
   }
 
-  void _updateStatus(List<ConnectivityResult> result) {
-    const String messageText = 'Lütfen internet bağlantınızı kontrol ediniz.';
-
-    if (result.contains(ConnectivityResult.none)) {
-      customSnackBar(messageText);
-    } else {
-      // Go to the home page
-      _goToTheHomePage();
-
-      if (Get.isSnackbarOpen) {
-        Get.closeCurrentSnackbar();
-      }
-    }
-  }
+  
 
   Future<dynamic> _goToTheHomePage() =>
       Future.delayed(const Duration(seconds: 3)).then(
---
+++ lib/src/product/network/network_controller.dart
@@ -0,0 +1,35 @@
+import 'package:btpro_flutter_case/src/product/widgets/custom_snackbar.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NetworkController extends GetxController {
+  final Connectivity _connectivity = Connectivity();
+  late final String? messageText;
+  late final VoidCallback? isConnected;
+
+  NetworkController({
+    this.isConnected,
+    this.messageText = 'Please check your internet connection',
+  });
+
+  @override
+  void onInit() {
+    super.onInit();
+    _connectivity.onConnectivityChanged.listen(_updateStatus);
+  }
+
+  void _updateStatus(List<ConnectivityResult> result) {
+    if (result.contains(ConnectivityResult.none)) {
+      customSnackBar(messageText ?? '');
+    } else {
+      if (isConnected != null) {
+        isConnected!();
+      }
+
+      if (Get.isSnackbarOpen) {
+        Get.closeCurrentSnackbar();
+      }
+    }
+  }
+}
