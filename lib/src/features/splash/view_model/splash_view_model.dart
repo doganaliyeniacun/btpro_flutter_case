901b9b52d2ec84b38931a87925c13bc09fba294c
Doğan Ali Yeniacun <alidoganyeniacun@gmail.com>
2024-06-21 21:23:28 +0300

    Update splash_view_model.dart

--- lib/src/features/splash/view_model/splash_view_model.dart
+++ lib/src/features/splash/view_model/splash_view_model.dart
@@ -1,5 +1,4 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
