95183ac66092dfed3511b44b70eaad3e2a4aaa77
Doğan Ali Yeniacun <alidoganyeniacun@gmail.com>
2024-06-25 15:48:03 +0300

    Update remote_config_service.dart

--- lib/src/product/firebase/remote_config/service/remote_config_service.dart
+++ lib/src/product/firebase/remote_config/service/remote_config_service.dart
@@ -24,6 +24,7 @@ class RemoteConfigService extends GetxController
     }
   }
 
+  /// This function sets configuration settings for remote configuration.
   Future<void> _setConfigSettings() async {
     _remoteConfig.setConfigSettings(
       RemoteConfigSettings(
@@ -33,10 +34,19 @@ class RemoteConfigService extends GetxController
     );
   }
 
+  /// This function fetches and activates remote configuration data asynchronously.
   Future<void> _fetchAndActivate() async {
     await _remoteConfig.fetchAndActivate();
   }
 
+  /// This function overrides the getString method to return a string value from a remote configuration
+  /// based on the provided configName.
+  /// 
+  /// Args:
+  ///   configName (String): The `configName` parameter is a string that represents the name of a
+  /// configuration setting or value that you want to retrieve from a remote configuration source. In
+  /// the provided code snippet, the `getString` method takes this `configName` as an argument and
+  /// retrieves the corresponding value from the `_remoteConfig
   @override
   String getString(String configName) => _remoteConfig.getString(configName);
 }
