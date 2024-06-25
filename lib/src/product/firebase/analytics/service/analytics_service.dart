89a6a96e443dae7a47a8e8b18b4ce2a02075a3b0
Doğan Ali Yeniacun <alidoganyeniacun@gmail.com>
2024-06-25 15:45:17 +0300

    Update analytics_service.dart

--- lib/src/product/firebase/analytics/service/analytics_service.dart
+++ lib/src/product/firebase/analytics/service/analytics_service.dart
@@ -6,6 +6,13 @@ class AnalyticsService {
   FirebaseAnalyticsObserver getAnalyticsObserver() =>
       FirebaseAnalyticsObserver(analytics: _analytics);  
 
+  /// The `logEvent` function logs an event with a specified name and parameters using an analytics
+  /// service.
+  /// 
+  /// Args:
+  ///   eventName (String): The `eventName` parameter is a required `String` that represents the name of
+  /// the event to be logged.
+  ///   parameters (Map<String, Object>): The `logEvent` function takes two required parameters:
   Future<void> logEvent({
     required String eventName,
     required Map<String, Object> parameters,
