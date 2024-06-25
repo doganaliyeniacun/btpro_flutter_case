a11114a2d8d3fa6349456181cbe5f8b3fb5923e3
Doğan Ali Yeniacun <alidoganyeniacun@gmail.com>
2024-06-25 15:45:35 +0300

    Update firebase_notifications_service.dart

--- lib/src/product/firebase/notifications/service/firebase_notifications_service.dart
+++ lib/src/product/firebase/notifications/service/firebase_notifications_service.dart
@@ -3,6 +3,8 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 class FirebaseNotificationsService {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
+  /// The `initNotifications` function in Dart initializes notifications by requesting permission from
+  /// Firebase Messaging.
   Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
   }
