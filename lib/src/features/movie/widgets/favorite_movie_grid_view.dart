1bad58ce31b75213cd990bb2d12788621e7d225c
Doğan Ali Yeniacun <alidoganyeniacun@gmail.com>
2024-06-29 13:31:51 +0300

    Update favorite_movie_grid_view.dart

--- lib/src/features/movie/widgets/favorite_movie_grid_view.dart
+++ lib/src/features/movie/widgets/favorite_movie_grid_view.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:btpro_flutter_case/src/features/movie/view/favorite_movie_view.dart';
 import 'package:btpro_flutter_case/src/features/movie/widgets/movie_grid_card.dart';
 import 'package:flutter/material.dart';
 
